84dd72ad5d9cd01c648acdf601937e4b55872eae
Daniel Kuzin <daniel@kuzin.dk>
2025-01-29 20:40:29 +0100

    Add fish, ghostty, starship, nvim

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,7 @@
+return {
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", name = "catppuccin", opts = {
+    transparent_background = true,
+  } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } },
+}
