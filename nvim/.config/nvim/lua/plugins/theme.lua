bf4197a9676365267d2b050e4f42403db57cf8ec
Daniel Kuzin <daniel@kuzin.dk>
2025-09-19 14:33:53 +0200

    fix: theming

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
-  { "catppuccin/nvim", name = "catppuccin", opts = {
-    transparent_background = true,
-  } },
-  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } },
+  { "catppuccin/nvim", name = "catppuccin", opts = {} },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
 }
